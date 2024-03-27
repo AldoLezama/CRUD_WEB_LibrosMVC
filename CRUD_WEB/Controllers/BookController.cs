b22b0112e1d217f842fa723b92e8ab97c599d8ff
Johan Balabarca <johan.balabarca@soltechsac.com>
2024-03-27 14:48:17 -0500

    Proyecto agregado

---
+++ CRUD_WEB/Controllers/BookController.cs
@@ -0,0 +1,164 @@
+﻿using CRUD_WEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_WEB.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            using (BookCRUDEntities context = new BookCRUDEntities())
+            {
+                return View(context.Libros.ToList());
+            }
+               
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            //using (BookCRUDEntities context = new BookCRUDEntities())
+            //{
+            //    return View(context.Libros.Where(x => x.idLibro == id));
+            //}
+            // Obtener el libro con el ID proporcionado
+            // GET: Book/Details/5
+            using (var context = new BookCRUDEntities())
+            {
+                // Obtener el libro con el ID proporcionado
+                var libro = context.Libros.FirstOrDefault(x => x.idLibro == id);
+
+                if (libro != null)
+                {
+                    return View(libro); // Pasar el objeto de libro a la vista
+                }
+                else
+                {
+                    return HttpNotFound(); // Devolver un error 404 si el libro no se encuentra
+                }
+            }
+
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            //using (BD_Models context = new BD_Models())
+            //{
+            //    return View(context.Libros.Where(x => x.idBook == id == id));
+            //}
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(Libros Libros)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (BookCRUDEntities context = new BookCRUDEntities())
+                {
+                    context.Libros.Add(Libros);
+                    context.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (var context = new BookCRUDEntities())
+            {
+                // Obtener el libro con el ID proporcionado
+                var libro = context.Libros.FirstOrDefault(x => x.idLibro == id);
+
+                if (libro != null)
+                {
+                    return View(libro); // Pasar el objeto de libro a la vista
+                }
+                else
+                {
+                    return HttpNotFound(); // Devolver un error 404 si el libro no se encuentra
+                }
+            }
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Libros books)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (BookCRUDEntities context = new BookCRUDEntities())
+                {
+                    context.Entry(books).State = System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            //using (BookCRUDEntities context = new BookCRUDEntities())
+            //{
+            //    return View(context.Libros.Where(x => x.idLibro == id));
+            //}
+            using (var context = new BookCRUDEntities())
+            {
+                // Obtener el libro con el ID proporcionado
+                var libro = context.Libros.FirstOrDefault(x => x.idLibro == id);
+
+                if (libro != null)
+                {
+                    return View(libro); // Pasar el objeto de libro a la vista
+                }
+                else
+                {
+                    return HttpNotFound(); // Devolver un error 404 si el libro no se encuentra
+                }
+            }
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (BookCRUDEntities context = new BookCRUDEntities())
+                {
+                    Libros book = context.Libros.Where(x => x.idLibro == id).FirstOrDefault();
+                    context.Libros.Remove(book);
+                    context.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUD_WEB/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("CRUD_WEB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CRUD_WEB")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el ID. typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("87d7db7b-ba43-4064-983f-a21dc8223018")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
